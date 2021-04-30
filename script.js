58d084a0c21dfc8d385378e76f3374a8e1b080bb
loserchik67 <loserchik67@gmail.com>
2021-04-30 16:01:26 -0500

    finish the game functionality

--- script.js
+++ script.js
@@ -12,15 +12,23 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-const scores = [0, 0];
-let activePlayer = 0;
-let currentScore = 0;
-let playing = true;
+let scores, activePlayer, currentScore, playing;
+
+const init = function () {
+  scores = [0, 0];
+  activePlayer = 0;
+  currentScore = 0;
+  playing = true;
+
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  diceEl.classList.add('hidden');
+};
 
 //Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+init();
 
 const switchPlayer = function () {
   currentScore = 0;
@@ -84,4 +92,11 @@ btnHold.addEventListener('click', function () {
   }
 });
 
-btnNew.addEventListener('click', function () {});
+btnNew.addEventListener('click', function () {
+  console.log('new game');
+  document
+    .querySelector(`.player--${activePlayer}`)
+    .classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  init();
+});
